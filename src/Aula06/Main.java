1650631686100cd52d3f59cdd8368322f433a2bc
Katherine Mariete <katamarieth@gmail.com>
2021-11-18 16:14:14 -0300

    primeiro commit

---
+++ src/Aula06/Main.java
@@ -0,0 +1,9 @@
+package Aula06;
+
+public class Main {
+    public static void main(String[] args) {
+    Cachorro cachorro = new Cachorro("dorado","Border Collie",8.0,2020,true,false);
+
+        System.out.println(cachorro);
+    }
+}
